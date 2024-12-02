f15cf41272427729d1fd7482ddf074c3a0bebb82
andrew <a.myrold@att.net>
2024-12-02 13:41:10 -0500

    Some minor changes to the scripts

--- backend/routes/index.ts
+++ backend/routes/index.ts
@@ -1,8 +1,8 @@
-import express from "express";
+import { Router } from "express";
 import apiRouter from "./api";
 import path from "node:path";
 
-const router = express.Router({ caseSensitive: true, strict: true });
+const router = Router({ caseSensitive: true, strict: true });
 
 router.use("/api", apiRouter);
 // Static routes
